b845a940c99b4c74fde4f57b4f3261b404fc8efd
Suyog Rao <suyog.rao@gmail.com>
2014-11-06 16:38:56 -0800

    Added inline code asciidoc formatting

--- lib/logstash/filters/translate.rb
+++ lib/logstash/filters/translate.rb
@@ -6,18 +6,18 @@ require "logstash/namespace"
 # and/or a YAML file to determine replacement values.
 #
 # The dictionary entries can be specified in one of two ways: First,
-# the "dictionary" configuration item may contain a hash representing
+# the `dictionary` configuration item may contain a hash representing
 # the mapping. Second, an external YAML file (readable by logstash) may be specified
-# in the "dictionary_path" configuration item. These two methods may not be used
+# in the `dictionary_path` configuration item. These two methods may not be used
 # in conjunction; it will produce an error.
 #
-# Operationally, if the event field specified in the "field" configuration
+# Operationally, if the event field specified in the `field` configuration
 # matches the EXACT contents of a dictionary entry key (or matches a regex if
-# "regex" configuration item has been enabled), the field's value will be substituted
+# `regex` configuration item has been enabled), the field's value will be substituted
 # with the matched key's value from the dictionary.
 #
 # By default, the translate filter will replace the contents of the 
-# maching event field (in-place). However, by using the "destination"
+# maching event field (in-place). However, by using the `destination`
 # configuration item, you may also specify a target event field to
 # populate with the new translated value.
 # 
@@ -29,7 +29,7 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   milestone 1
 
   # The name of the logstash event field containing the value to be compared for a
-  # match by the translate filter (e.g. "message", "host", "response_code"). 
+  # match by the translate filter (e.g. `message`, `host`, `response_code`). 
   # 
   # If this field is an array, only the first value will be used.
   config :field, :validate => :string, :required => true
@@ -40,9 +40,9 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   config :override, :validate => :boolean, :default => false
 
   # The dictionary to use for translation, when specified in the logstash filter
-  # configuration item (i.e. do not use the @dictionary_path YAML file)
+  # configuration item (i.e. do not use the `@dictionary_path` YAML file)
   # Example:
-  #
+  # [source,ruby]
   #     filter {
   #       %PLUGIN% {
   #         dictionary => [ "100", "Continue",
@@ -51,19 +51,19 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   #                         "old version", "new version" ]
   #       }
   #     }
-  # NOTE: it is an error to specify both dictionary and dictionary_path
+  # NOTE: it is an error to specify both `dictionary` and `dictionary_path`
   config :dictionary, :validate => :hash,  :default => {}
 
   # The full path of the external YAML dictionary file. The format of the table
   # should be a standard YAML file. Make sure you specify any integer-based keys
   # in quotes. The YAML file should look something like this:
-  #
+  # [source,ruby]
   #     "100": Continue
   #     "101": Switching Protocols
   #     merci: gracias
   #     old version: new version
   #     
-  # NOTE: it is an error to specify both dictionary and dictionary_path
+  # NOTE: it is an error to specify both `dictionary` and `dictionary_path`
   config :dictionary_path, :validate => :path
 
   # When using a dictionary file, this setting will indicate how frequently
@@ -71,7 +71,7 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   config :refresh_interval, :validate => :number, :default => 300
   
   # The destination field you wish to populate with the translated code. The default
-  # is a field named "translation". Set this to the same value as source if you want
+  # is a field named `translation`. Set this to the same value as source if you want
   # to do a substitution, in this case filter will allways succeed. This will clobber
   # the old value of the source field! 
   config :destination, :validate => :string, :default => "translation"
@@ -82,14 +82,15 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   # destination field's data, with the translated value substituted in-place.
   #
   # For example, consider this simple translation.yml, configured to check the `data` field:
+  # [source,ruby]
   #     foo: bar
   #
-  # If logstash receives an event with the `data` field set to "foo", and `exact => true`,
-  # the destination field will be populated with the string "bar".
+  # If logstash receives an event with the `data` field set to `foo`, and `exact => true`,
+  # the destination field will be populated with the string `bar`.
   
   # If `exact => false`, and logstash receives the same event, the destination field
-  # will be also set to "bar". However, if logstash receives an event with the `data` field
-  # set to "foofing", the destination field will be set to "barfing".
+  # will be also set to `bar`. However, if logstash receives an event with the `data` field
+  # set to `foofing`, the destination field will be set to `barfing`.
   #
   # Set both `exact => true` AND `regex => `true` if you would like to match using dictionary
   # keys as regular expressions. A large dictionary could be expensive to match in this case. 
@@ -100,15 +101,15 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
   config :regex, :validate => :boolean, :default => false
 
   # In case no translation occurs in the event (no matches), this will add a default
-  # translation string, which will always populate "field", if the match failed.
+  # translation string, which will always populate `field`, if the match failed.
   #
   # For example, if we have configured `fallback => "no match"`, using this dictionary:
-  #
+  # [source,ruby]
   #     foo: bar
   #
-  # Then, if logstash received an event with the field `foo` set to "bar", the destination
-  # field would be set to "bar". However, if logstash received an event with `foo` set to "nope",
-  # then the destination field would still be populated, but with the value of "no match".
+  # Then, if logstash received an event with the field `foo` set to `bar`, the destination
+  # field would be set to `bar`. However, if logstash received an event with `foo` set to `nope`,
+  # then the destination field would still be populated, but with the value of `no match`.
   config :fallback, :validate => :string
 
   public
