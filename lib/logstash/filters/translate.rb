c2a509921546121f05c6e4484353efbfc05a90fe
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-10-07 11:12:29 -0400

    remove filter

--- lib/logstash/filters/translate.rb
+++ lib/logstash/filters/translate.rb
@@ -148,7 +148,7 @@ class LogStash::Filters::Translate < LogStash::Filters::Base
 
   public
   def filter(event)
-    return unless filter?(event)
+    
 
     if @dictionary_path
       if @next_refresh < Time.now
