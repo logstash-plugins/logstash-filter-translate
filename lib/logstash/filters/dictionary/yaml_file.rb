d9d5bbbd2402273a8f9c953211003c3a7087fa2a
João Duarte <jsvd@users.noreply.github.com>
2025-07-24 09:13:01 +0100

    allow yaml parser and visitor to be garbage collected (#104)
    When loading a YAML dictionary the YamlFile class will store a YAML visitor and a parser.
    
    This means that after "read_file_into_dictionary" is called the Psych:Parser will keep the object tree that was used to parse and navigate the YAML document, even after the resulting Hash dictionary is created.
    
    While the memory required to parse the YAML dictionary is still required, we can change the YamlFile class to allow the Psych::Parser garbage collection, freeing a significant amount of memory from the heap.

--- lib/logstash/filters/dictionary/yaml_file.rb
+++ lib/logstash/filters/dictionary/yaml_file.rb
@@ -8,19 +8,19 @@ module LogStash module Filters module Dictionary
     protected
 
     def initialize_for_file_type(**file_type_args)
-      @visitor = YamlVisitor.create
-
-      @parser = Psych::Parser.new(Psych::TreeBuilder.new)
-      @parser.code_point_limit = file_type_args[:yaml_code_point_limit]
+      @yaml_code_point_limit = file_type_args[:yaml_code_point_limit]
     end
 
     def read_file_into_dictionary
+      visitor = YamlVisitor.create
+      parser = Psych::Parser.new(Psych::TreeBuilder.new)
+      parser.code_point_limit = @yaml_code_point_limit
       # low level YAML read that tries to create as
       # few intermediate objects as possible
       # this overwrites the value at key
       yaml_string = IO.read(@dictionary_path, :mode => 'r:bom|utf-8')
-      @parser.parse(yaml_string, @dictionary_path)
-      @visitor.accept_with_dictionary(@dictionary, @parser.handler.root)
+      parser.parse(yaml_string, @dictionary_path)
+      visitor.accept_with_dictionary(@dictionary, parser.handler.root)
     end
   end
 end end end
